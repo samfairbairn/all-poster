6ce6da0607c832d1d258b0e69f77393cc3ec954b
Sam Fairbairn <samfairbairn@Sams-MacBook-Pro-2.local>
2019-08-25 00:34:58 +0800

    add god rays

--- src/services/threeService.js
+++ src/services/threeService.js
@@ -8,12 +8,29 @@ var fragmentShader = require("../shaders/shader.fs");
 import bearTextureImg from '@/assets/bear1Imagem1.png'
 import ballTextureImg from '@/assets/ballImagem1.png'
 
+import {
+  EffectComposer,
+  RenderPass,
+	BlendFunction,
+	EffectPass,
+	GodRaysEffect,
+  BloomEffect,
+	KernelSize,
+	SMAAEffect
+} from 'postprocessing'
+
 class threeService {
   constructor(el) {
 
     this.$el = el
     console.log('this.$el', this.$el);
-    
+
+    this.composer
+    this.renderPass
+    this.effectPass
+    this.light
+    this.sun
+
     this.container
     this.controls
     this.renderer
@@ -26,13 +43,24 @@ class threeService {
     this.fov = 30
 
     this.setup()
-    
+
   }
 
   setup = () => {
 
     this.container = this.$el;
-    this.scene = new THREE.Scene();
+
+    // setup renderer
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.renderer.setPixelRatio( window.devicePixelRatio );
+
+    this.container.appendChild( this.renderer.domElement );
+    this.scene = new THREE.Scene()
+    this.clock = new THREE.Clock()
+
+    // camera
 
     this.camera = new THREE.PerspectiveCamera(
       this.fov,
@@ -41,13 +69,101 @@ class threeService {
       10000
     );
     this.camera.position.z = 400;
-    
+
+    // composer
+
+    this.composer = new EffectComposer(this.renderer)
+    this.renderPass = new RenderPass(this.scene, this.camera)
+    this.renderPass.renderToScreen = false
+
+    // Light.
+
+    const ambientLight = new THREE.AmbientLight(0x808080)
+		const directionalLight = new THREE.DirectionalLight(0xffbbaa)
+		// directionalLight.position.set(75, 25, -100)
+
+    // set light inside the ball
+		// directionalLight.position.set(0, 0, 0)
+    // set light inside the bear
+		// directionalLight.position.set(0, 0, -50)
+
+		directionalLight.target = this.scene
+		this.light = directionalLight
+		this.scene.add(ambientLight)
+		this.scene.add(directionalLight)
+
+    // Sun.
+
+		const sunMaterial = new THREE.MeshBasicMaterial({
+			color: 0xffddaa,
+			transparent: true,
+			fog: false
+		});
+
+		const sunGeometry = new THREE.SphereBufferGeometry(32, 32, 32);
+		const sun = new THREE.Mesh(sunGeometry, sunMaterial);
+		sun.frustumCulled = false;
+
+		const group = new THREE.Group();
+		group.position.copy(this.light.position);
+		group.add(sun);
+
+		// The sun mesh is not added to the scene to hide hard geometry edges.
+		// scene.add(group);
+		sun.matrixAutoUpdate = false;
+		this.sun = sun;
+
+    // god rays
+
+    const godRaysEffect = new GodRaysEffect(this.camera, this.sun, {
+  			height: 720,
+  			kernelSize: KernelSize.SMALL,
+  			density: 0.96,
+  			decay: 0.96,
+  			weight: 0.3,
+  			exposure: 0.54,
+  			samples: 60,
+  			clampMax: 1.0
+  		});
+
+		godRaysEffect.dithering = true;
+		// this.effect = godRaysEffect;
+
+    // this.effectPass = new EffectPass(this.camera, new BloomEffect());
+    this.effectPass = new EffectPass(this.camera, godRaysEffect)
+    this.effectPass.renderToScreen = true
+
+    // add render passes
+
+    this.composer.addPass(this.renderPass);
+    this.composer.addPass(this.effectPass);
+
+    // choose object
+
+    const objectId = 'ball'
+    let objectTexture, objectModel
+
+    switch (objectId) {
+      case 'ball':
+        objectTexture = ballTextureImg
+        objectModel = '/staticAssets/ball.obj'
+        break
+      case 'bear':
+      default:
+        objectTexture = bearTextureImg
+        objectModel = '/staticAssets/bear1.obj'
+        break
+    }
+
+    // load object
+
     this.material = new THREE.ShaderMaterial( {
       uniforms: {
         tGrid: {
           type: "t",
           // value: THREE.ImageUtils.loadTexture(bearTextureImg)
-          value: THREE.ImageUtils.loadTexture(ballTextureImg)
+          // value: THREE.ImageUtils.loadTexture(ballTextureImg)
+          value: THREE.ImageUtils.loadTexture(objectTexture)
         },
         time: {
           type: "f",
@@ -68,23 +184,41 @@ class threeService {
 
     var loader = new OBJLoader( manager );
     // loader.load( '/staticAssets/bear1.obj', ( obj ) => {
-    loader.load( '/staticAssets/ball.obj', ( obj ) => {
+    // loader.load( '/staticAssets/ball.obj', ( obj ) => {
+    loader.load( objectModel, ( obj ) => {
       this.object = obj;
     }, this.onProgress, this.onError );
-    
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
-    this.renderer.setPixelRatio( window.devicePixelRatio );
 
-    this.container.appendChild( this.renderer.domElement );
+    // setup orbit
 
-    // this.controls = new THREE.OrbitControls( this.camera, this.renderer.domElement );
     this.controls = new OrbitControls( this.camera, this.renderer.domElement );
+
+    // resize
 
     this.onWindowResize();
     window.addEventListener( 'resize', this.onWindowResize );
 
+    // render
+
     this.render();
+  }
+
+  initObject = (object = 'bear') => {
+
+    let objectTexture, objectModel
+
+    switch (object) {
+      case 'ball':
+        objectTexture = ballTextureImg
+        objectModel = '/staticAssets/ball.obj'
+        break
+      case 'bear':
+      default:
+        objectTexture = bearTextureImg
+        objectModel = '/staticAssets/bear1.obj'
+        break
+    }
+
   }
 
   loadModel = () => {
@@ -113,7 +247,8 @@ class threeService {
 
   render = () => {
     this.material.uniforms[ 'time' ].value = .00025 * ( Date.now() - this.start );
-    this.renderer.render( this.scene, this.camera );
+    // this.renderer.render( this.scene, this.camera );
+    this.composer.render(this.clock.getDelta());
     requestAnimationFrame( this.render );
   }
 }
